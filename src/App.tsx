00daeede349b7786b744226c66c0198966cc1aae
jordan <jordan.lesich@gmail.com>
2025-01-08 21:23:36 -0800

    add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
+import '@mantine/core/styles.css';
+import { Button, Container, MantineProvider, Stack } from '@mantine/core';
+import { theme } from './theme';
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <Container h="100vh">
+        <Stack h="100%" justify="center" align="center">
+          <Button>Click me, daddy</Button>
+        </Stack>
+      </Container>
+    </MantineProvider>
+  );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,59 @@
-import { createTheme } from "@mantine/core";
+import { createTheme } from '@mantine/core';
+
+const KELP = [
+  '#e9f5f4',
+  '#d5edec',
+  '#9dd1ce',
+  '#6bb3ad',
+  '#257a72',
+  '#00433b', // root color
+  '#003b31',
+  '#003025',
+  '#00291d',
+  '#001f13',
+  '#00140c',
+] as const;
+
+// E2E8F0
+
+const DARK = [
+  '#f2f2f2',
+  '#E2E8F0', // common grey
+  '#c4c4c4',
+  '#929695', // secondary text color
+  '#595959',
+  '#111111', // common text color
+  '#0f0e0e',
+  '#0d0a0a',
+  '#0a0606',
+  '#080303',
+  '#050101',
+] as const;
+
+const PURPLE = [
+  '#f9f5fc',
+  '#f5ebfc',
+  '#e4cdf7',
+  '#d0b1f2',
+  '#a37aeb',
+  '#6e48e0',
+  '#5c3ac9',
+  '#4428a8',
+  '#301a87',
+  '#1e0e66',
+  '#100642',
+] as const;
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  fontFamily: 'DM Sans, sans-serif',
+  defaultRadius: 'sm',
+  colors: {
+    kelp: KELP,
+    dark: DARK,
+    purple: PURPLE,
+  },
+
+  components: {
+    Text: {},
+  },
 });
