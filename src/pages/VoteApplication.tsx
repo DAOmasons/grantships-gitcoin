cd26cbfc20a56694d62056d4750ce7fb0c7ee787
jordan <jordan.lesich@gmail.com>
2025-01-14 22:13:56 -0800

    integrate user application data

--- src/pages/VoteApplication.tsx
+++ src/pages/VoteApplication.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { PageLayout } from '../layout/Page';
 import {
   Avatar,
   Box,
   Button,
+  Card,
+  Collapse,
   Divider,
   Group,
   InputLabel,
@@ -15,12 +17,37 @@ import {
   useMantineTheme,
 } from '@mantine/core';
 import { Question, RUBRIC_COPY, RubricSection } from '../constants/rubric';
-import { IconChevronDown } from '@tabler/icons-react';
+import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import { useChews } from '../hooks/useChews';
+import { useParams } from 'react-router-dom';
+import { RoundApplicationContent } from '../constants/dummyApplications';
+import { useDisclosure } from '@mantine/hooks';
 
 export const VoteApplication = () => {
+  const { id } = useParams();
   const [step, setStep] = useState(0);
   const [scores, setScores] = useState<Record<string, number>>({});
   const [feedback, setFeedback] = useState<Record<string, string>>({});
+
+  const { applicationRound, isLoadingAppRound } = useChews();
+
+  const currentApplication = applicationRound?.applications.find(
+    (app) => app.id === id
+  );
+
+  const appCopy = currentApplication?.copy;
+
+  if (isLoadingAppRound) {
+    return null;
+  }
+
+  if (!appCopy) {
+    return (
+      <PageLayout title="Application Vote">
+        <Text>Application not found</Text>
+      </PageLayout>
+    );
+  }
 
   const handleChangeScore = (key: string, value: number) => {
     setScores({ ...scores, [key]: value });
@@ -49,6 +76,7 @@ export const VoteApplication = () => {
                 setStep={setStep}
                 index={index}
                 totalSteps={RUBRIC_COPY.sections.length}
+                appCopy={appCopy as RoundApplicationContent}
               />
             </Stepper.Step>
           );
@@ -67,6 +95,7 @@ const RubricStep = ({
   index,
   setStep,
   totalSteps,
+  appCopy,
 }: {
   section: RubricSection;
   scores: Record<string, number>;
@@ -76,6 +105,7 @@ const RubricStep = ({
   index: number;
   setStep: (step: number) => void;
   totalSteps?: number;
+  appCopy: RoundApplicationContent;
 }) => {
   const theme = useMantineTheme();
 
@@ -101,6 +131,7 @@ const RubricStep = ({
             scores={scores}
             key={question.title}
             setScores={setScores}
+            appCopy={appCopy}
           />
         ))}
       </Box>
@@ -112,6 +143,7 @@ const RubricStep = ({
         <Textarea
           key={section.sectionName}
           value={feedback}
+          placeholder="Enter feedback here..."
           onChange={(e) => setFeedback(section.sectionName, e.target.value)}
         />
       </Box>
@@ -139,12 +171,21 @@ const RubricQuestion = ({
   question,
   setScores,
   scores,
+  appCopy,
 }: {
   scores: Record<string, number>;
   question: Question;
   setScores: (key: string, value: number) => void;
+  appCopy: RoundApplicationContent;
 }) => {
   const { colors } = useMantineTheme();
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const applicantResponse = useMemo(() => {
+    return appCopy.responses.find(
+      (response) => response.title === question.title
+    );
+  }, [appCopy, question.title]);
 
   return (
     <Box mb="xxl">
@@ -171,15 +212,20 @@ const RubricQuestion = ({
         </Text>
         <Avatar fz={24} />
         <Text fz="lg" c={colors.dark[4]}>
-          mask.eth
+          {appCopy.roundName}
         </Text>
-        <Group gap={4} style={{ cursor: 'pointer' }} onClick={() => {}}>
+        <Group gap={4} style={{ cursor: 'pointer' }} onClick={() => toggle()}>
           <Text c={colors.dark[4]} fz="lg" td="underline" fw={500}>
             Expand
           </Text>
-          <IconChevronDown />
+          {opened ? <IconChevronUp /> : <IconChevronDown />}
         </Group>
       </Group>
+      <Collapse in={opened}>
+        <Card variant="inner" mt={'md'}>
+          <Text c={colors.dark[4]}>{applicantResponse?.response}</Text>
+        </Card>
+      </Collapse>
     </Box>
   );
 };
