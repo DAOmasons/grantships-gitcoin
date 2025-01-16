ada1605db0c9c2ff125e82ce28fec70a24385b02
jordan <jordan.lesich@gmail.com>
2025-01-15 20:17:58 -0800

    display avgs, progress bar, leader icons, sort

--- src/pages/Applications.tsx
+++ src/pages/Applications.tsx
@@ -11,12 +11,61 @@ import {
 import { PageLayout } from '../layout/Page';
 import { AddressAvatar } from '../components/AddressAvatar';
 import { useChews } from '../hooks/useChews';
-import { Address } from 'viem';
+import { Address, formatEther } from 'viem';
 import fxClasses from '../style/effects.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useMemo } from 'react';
+import {
+  IconNumber2,
+  IconNumber3,
+  IconNumber4,
+  IconNumber5,
+  IconNumber6,
+  IconSquareNumber1,
+  IconSquareNumber2,
+  IconSquareNumber3,
+  IconSquareNumber4,
+  IconSquareNumber5,
+  IconSquareNumber6,
+} from '@tabler/icons-react';
+
+const icons = [
+  IconSquareNumber1,
+  IconSquareNumber2,
+  IconSquareNumber3,
+  IconSquareNumber4,
+  IconSquareNumber5,
+  IconSquareNumber6,
+];
 
 export const Applications = () => {
   const { colors } = useMantineTheme();
   const { applicationRound } = useChews();
+
+  const navigate = useNavigate();
+
+  const transformed = useMemo(() => {
+    return applicationRound?.applications
+      .map((app) => {
+        const avgScore =
+          app?.votes.length === 0
+            ? '0'
+            : formatEther(
+                (app.votes.reduce(
+                  (acc, vote) => acc + BigInt(vote.amount),
+                  0n
+                ) /
+                  BigInt(app.votes.length)) *
+                  100n
+              );
+
+        return {
+          ...app,
+          avgScore,
+        };
+      })
+      .sort((a, b) => Number(b.avgScore) - Number(a.avgScore));
+  }, [applicationRound]);
 
   return (
     <PageLayout title="Applications">
@@ -27,39 +76,57 @@ export const Applications = () => {
         Review application status and comments from the Judges
       </Text>
       <Box>
-        {applicationRound?.applications.map((app) => (
-          <Group
-            px="lg"
-            py="sm"
-            mb={32}
-            key={app.id}
-            justify="space-between"
-            className={fxClasses.hoverCard}
-          >
-            <Group>
-              <AddressAvatar size={56} address={app.registrar as Address} />
-              <Box>
-                <Text fw={600} fz="lg" mb={4} maw={240} lineClamp={1}>
-                  {app.copy.roundName}
-                </Text>
-                <Text c="subtle">Last Updated Jan 1, 2025</Text>
-              </Box>
-            </Group>
-            <Stack gap={0} align="flex-end">
-              <Group mb={4} gap={8}>
-                <Text c="subtle">Total 35/40</Text>
-                <Progress
-                  w={160}
-                  h={8}
-                  value={80}
-                  color={colors.purple[6]}
-                  bg={colors.dark[2]}
-                />
+        {transformed?.map((app, index) => {
+          const Icon = index < icons.length ? icons[index] : null;
+          return (
+            <Group
+              px="lg"
+              py="sm"
+              mb={32}
+              key={app.id}
+              justify="space-between"
+              className={fxClasses.hoverCard}
+              onClick={() => navigate(`/view-application/${app.id}`)}
+            >
+              <Group>
+                <Box pos="relative">
+                  {Icon && (
+                    <Icon
+                      size={24}
+                      fill={'white'}
+                      style={{
+                        position: 'absolute',
+                        top: -2,
+                        right: -5,
+                        zIndex: 1,
+                      }}
+                    />
+                  )}
+                  <AddressAvatar size={56} address={app.registrar as Address} />
+                </Box>
+                <Box>
+                  <Text fw={600} fz="lg" mb={4} maw={240} lineClamp={1}>
+                    {app.copy.roundName}
+                  </Text>
+                  <Text c="subtle">Last Updated Jan 1, 2025</Text>
+                </Box>
               </Group>
-              <Text c="subtle">Currently {app.amountReviewed} Voted</Text>
-            </Stack>
-          </Group>
-        ))}
+              <Stack gap={0} align="flex-end">
+                <Group mb={4} gap={8}>
+                  <Text c="subtle">Avg. {app.avgScore} %</Text>
+                  <Progress
+                    w={160}
+                    h={8}
+                    value={Number(app.avgScore)}
+                    color={colors.purple[6]}
+                    bg={colors.dark[2]}
+                  />
+                </Group>
+                <Text c="subtle">Currently {app.amountReviewed} Voted</Text>
+              </Stack>
+            </Group>
+          );
+        })}
       </Box>
     </PageLayout>
   );
