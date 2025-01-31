d2c0fe1f14c3c3caa125c8d65007c46ce1847881
jordan <jordan.lesich@gmail.com>
2025-01-31 01:11:07 -0800

    hook up stepper form to state

--- src/pages/SubmitApplicationAlt.tsx
+++ src/pages/SubmitApplicationAlt.tsx
@@ -2,7 +2,10 @@ import {
   Avatar,
   Box,
   Button,
+  Card,
+  Checkbox,
   Group,
+  InputLabel,
   Stack,
   Stepper,
   Text,
@@ -19,13 +22,31 @@ import {
   IconBrandTelegram,
   IconBrandX,
   IconCheck,
+  IconExclamationCircle,
   IconLink,
   IconMail,
   IconPhoto,
 } from '@tabler/icons-react';
-import { useForm } from '@mantine/form';
+import { useForm, UseFormReturnType, zodResolver } from '@mantine/form';
 import { useChews } from '../hooks/useChews';
 import { QUESTION_DESCRIPTIONS } from '../constants/rubric';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  roundName: z.string().min(1, 'Round name is required'),
+  imgUrl: z.string().url('Please enter a valid URL'),
+  responses: z.record(z.string(), z.string()),
+  email: z.string().email('Please enter a valid email'),
+  x: z.string().min(1, 'X username is required'),
+  discord: z.string().optional(),
+  github: z.string().optional(),
+  telegram: z.string().optional(),
+  autoEnroll: z.boolean(),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+type Form = UseFormReturnType<FormValues, (values: FormValues) => FormValues>;
 
 export const SubmitApplicationAlt = () => {
   const [step, setStep] = useState(0);
@@ -33,19 +54,27 @@ export const SubmitApplicationAlt = () => {
   const form = useForm({
     initialValues: {
       roundName: '',
-      address: '',
       imgUrl: '',
-      responses: [],
+      responses: {},
       email: '',
       x: '',
       discord: '',
       github: '',
       telegram: '',
+      autoEnroll: true,
     },
+    validateInputOnBlur: true,
+    validate: zodResolver(formSchema),
   });
   const { colors } = useMantineTheme();
 
   const appCopy = applicationRound?.rubric;
+
+  const appDetailsAnswered =
+    form.values.roundName &&
+    form.values.imgUrl &&
+    form.values.email &&
+    form.values.x;
 
   return (
     <PageLayout title="Submit Application">
@@ -58,8 +87,74 @@ export const SubmitApplicationAlt = () => {
             sectionName="Applicant Details"
             step={step}
             setStep={setStep}
+            disabled={!appDetailsAnswered}
           >
-            <ApplicantDetails />
+            <Stack gap="lg" mb={100}>
+              <Group justify="center">
+                <Avatar
+                  src={form.values.imgUrl || ''}
+                  bg={colors.dark[2]}
+                  size={171}
+                  mb="xl"
+                >
+                  <IconPhoto size={72} color={colors.dark[5]} />
+                </Avatar>
+              </Group>
+              <TextInput
+                label="Name"
+                required
+                placeholder="Grant Program Name"
+                {...form.getInputProps('roundName')}
+              />
+              <TextInput
+                label="Profile Photo URL"
+                required
+                leftSection={<IconLink color={colors.dark[4]} />}
+                placeholder="https://example.com/profile.jpg"
+                {...form.getInputProps('imgUrl')}
+              />
+              <Group wrap="nowrap" gap="sm" align="start">
+                <TextInput
+                  label="Email"
+                  required
+                  w={'50%'}
+                  placeholder="example@yesmail.xyz"
+                  leftSection={<IconMail color={colors.dark[4]} />}
+                  {...form.getInputProps('email')}
+                />
+                <TextInput
+                  label="X"
+                  required
+                  w={'50%'}
+                  placeholder="@example"
+                  leftSection={<IconBrandX color={colors.dark[4]} />}
+                  {...form.getInputProps('x')}
+                />
+              </Group>
+              <Group wrap="nowrap" gap="sm" align="start">
+                <TextInput
+                  label="Discord"
+                  w={'50%'}
+                  placeholder="https://discord.gg/example"
+                  leftSection={<IconBrandDiscord color={colors.dark[4]} />}
+                  {...form.getInputProps('discord')}
+                />
+                <TextInput
+                  label="GitHub"
+                  w={'50%'}
+                  placeholder="@example"
+                  leftSection={<IconBrandGithub color={colors.dark[4]} />}
+                  {...form.getInputProps('github')}
+                />
+              </Group>
+              <TextInput
+                label="Telegram"
+                w={'50%'}
+                placeholder="https://t.me/"
+                leftSection={<IconBrandTelegram color={colors.dark[4]} />}
+                {...form.getInputProps('telegram')}
+              />
+            </Stack>
           </StepLayout>
         </Stepper.Step>
         {appCopy?.sections.map((section, index) => (
@@ -71,9 +166,9 @@ export const SubmitApplicationAlt = () => {
             >
               {section.questions.map((question) => (
                 <Box key={question.title}>
-                  <Text fw={600} mb={8}>
+                  <InputLabel fz="md" fw={600} mb={8} required>
                     {question.title}
-                  </Text>
+                  </InputLabel>
                   <Text c="subtle" mb="sm">
                     {QUESTION_DESCRIPTIONS[question.title]}
                   </Text>
@@ -83,9 +178,40 @@ export const SubmitApplicationAlt = () => {
                     rows={3}
                     autosize={false}
                     mb="lg"
+                    {...form.getInputProps(`responses.${question.title}`)}
                   />
                 </Box>
               ))}
+              {index === appCopy.sections.length - 1 && (
+                <Stack>
+                  <Checkbox
+                    label="Automatically enroll me for GG23"
+                    color={colors.kelp[6]}
+                    iconColor={colors.dark[6]}
+                    checked={form.values.autoEnroll}
+                  />
+                  <Card variant="solid">
+                    <Group wrap="nowrap" gap={'xs'}>
+                      <Box
+                        component="span"
+                        style={{ transform: 'translateY(-20px)' }}
+                      >
+                        <IconExclamationCircle
+                          size={28}
+                          stroke={1.5}
+                          color={colors.kelp[6]}
+                        />
+                      </Box>
+                      <Text fz="sm">
+                        The wallet you are signing this transaction with will be
+                        the primary address for your profile. Please ensure that
+                        you are connected to a wallet that you plan to use and
+                        access during the duration of the program.
+                      </Text>
+                    </Group>
+                  </Card>
+                </Stack>
+              )}
             </StepLayout>
           </Stepper.Step>
         ))}
@@ -119,7 +245,7 @@ const StepLayout = ({
       {step !== 0 && (
         <Text mb={'lg'}>Please provide an answer for each question</Text>
       )}
-      {children}
+      <Box mx="32">{children}</Box>
       <Box mt={70}>
         <Group justify="center" gap="xl">
           <Button
@@ -141,73 +267,5 @@ const StepLayout = ({
         </Group>
       </Box>
     </Box>
-  );
-};
-
-const ApplicantDetails = () => {
-  const { colors } = useMantineTheme();
-
-  return (
-    <Stack gap="lg" mb={100}>
-      <Group justify="center">
-        <Avatar src={''} bg={colors.dark[2]} size={171} mb="xl">
-          <IconPhoto size={72} color={colors.dark[5]} />
-        </Avatar>
-      </Group>
-      <Group wrap="nowrap" gap="sm">
-        <TextInput
-          label="Name"
-          required
-          w={'50%'}
-          placeholder="Grant Program Name"
-        />
-        <TextInput label="Account" required w={'50%'} placeholder="0x1234..." />
-      </Group>
-      <TextInput
-        label="Profile Photo URL"
-        required
-        leftSection={<IconLink color={colors.dark[4]} />}
-        placeholder="https://example.com/profile.jpg"
-      />
-      {/* <Text fw={700} fz="xl">
-        Socials
-      </Text> */}
-      <Group wrap="nowrap" gap="sm">
-        <TextInput
-          label="Email"
-          required
-          w={'50%'}
-          placeholder="example@yesmail.xyz"
-          leftSection={<IconMail color={colors.dark[4]} />}
-        />
-        <TextInput
-          label="X"
-          required
-          w={'50%'}
-          placeholder="@example"
-          leftSection={<IconBrandX color={colors.dark[4]} />}
-        />
-      </Group>
-      <Group wrap="nowrap" gap="sm">
-        <TextInput
-          label="Discord"
-          w={'50%'}
-          placeholder="https://discord.gg/example"
-          leftSection={<IconBrandDiscord color={colors.dark[4]} />}
-        />
-        <TextInput
-          label="GitHub"
-          w={'50%'}
-          placeholder="@example"
-          leftSection={<IconBrandGithub color={colors.dark[4]} />}
-        />
-      </Group>
-      <TextInput
-        label="Telegram"
-        w={'50%'}
-        placeholder="https://t.me/"
-        leftSection={<IconBrandTelegram color={colors.dark[4]} />}
-      />
-    </Stack>
   );
 };
