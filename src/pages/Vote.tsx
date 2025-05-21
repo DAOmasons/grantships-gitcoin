592f0d1d04b0f78adf452cf4beef97e6aa39dde6
jordan <jordan.lesich@gmail.com>
2025-05-20 17:59:57 -0700

    missed changes

--- src/pages/Vote.tsx
+++ src/pages/Vote.tsx
@@ -17,66 +17,66 @@ import { Bold } from '../components/typography';
 const VOTE_TIME = 1747936800;
 
 export const Vote = () => {
-  // const [currentTab, setCurrentTab] = useState('vote');
+  const [currentTab, setCurrentTab] = useState('vote');
 
-  const [_tick, setTick] = useState(0);
+  // const [_tick, setTick] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTick((prev) => prev + 1);
-    }, 1000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setTick((prev) => prev + 1);
+  //   }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <PageLayout title="Vote">
-      <Box mb={'lg'}>
-        <Title order={3} fz={'h3'} mb={'xl'}>
-          Voting Will be Live {getTimeFromNow(VOTE_TIME)}
-        </Title>
-        <Group justify="center">
-          <Avatar src={gtcLogo} size={171} mb="xl" />
-        </Group>
-        <Text c="subtle" mb="sm"></Text>
-        <Text mb="sm">
-          In order to be eligible, you must hold at least{' '}
-          <Bold>100 GTC tokens on mainnet</Bold> before May 21st 2024. Voting
-          will take place on <Bold>Arbitrum</Bold>
-        </Text>
-      </Box>
-    </PageLayout>
-  );
+  //   return () => clearInterval(interval);
+  // }, []);
 
   // return (
   //   <PageLayout title="Vote">
-  //     <Tabs
-  //       defaultValue={currentTab}
-  //       value={currentTab}
-  //       mb={70}
-  //       onChange={(e) => (e ? setCurrentTab(e) : undefined)}
-  //     >
-  //       <Tabs.List mb="md">
-  //         <Tabs.Tab value="vote" leftSection={<IconStar size={14} />}>
-  //           Vote
-  //         </Tabs.Tab>
-  //         <Tabs.Tab value="results" leftSection={<IconTrophy size={14} />}>
-  //           Results
-  //         </Tabs.Tab>
-  //         <Tabs.Tab value="reviews" leftSection={<IconUsersGroup size={14} />}>
-  //           Reviews
-  //         </Tabs.Tab>
-  //       </Tabs.List>
-  //       <Tabs.Panel value="vote">
-  //         <VoteUI setCurrentTab={setCurrentTab} />
-  //       </Tabs.Panel>
-  //       <Tabs.Panel value="results">
-  //         <Results />
-  //       </Tabs.Panel>
-  //       <Tabs.Panel value="reviews">
-  //         <Reviews />
-  //       </Tabs.Panel>
-  //     </Tabs>
+  //     <Box mb={'lg'}>
+  //       <Title order={3} fz={'h3'} mb={'xl'}>
+  //         Voting Will be Live {getTimeFromNow(VOTE_TIME)}
+  //       </Title>
+  //       <Group justify="center">
+  //         <Avatar src={gtcLogo} size={171} mb="xl" />
+  //       </Group>
+  //       <Text c="subtle" mb="sm"></Text>
+  //       <Text mb="sm">
+  //         In order to be eligible, you must hold at least{' '}
+  //         <Bold>100 GTC tokens on mainnet</Bold> before May 21st 2024. Voting
+  //         will take place on <Bold>Arbitrum</Bold>
+  //       </Text>
+  //     </Box>
   //   </PageLayout>
   // );
+
+  return (
+    <PageLayout title="Vote">
+      <Tabs
+        defaultValue={currentTab}
+        value={currentTab}
+        mb={70}
+        onChange={(e) => (e ? setCurrentTab(e) : undefined)}
+      >
+        <Tabs.List mb="md">
+          <Tabs.Tab value="vote" leftSection={<IconStar size={14} />}>
+            Vote
+          </Tabs.Tab>
+          <Tabs.Tab value="results" leftSection={<IconTrophy size={14} />}>
+            Results
+          </Tabs.Tab>
+          <Tabs.Tab value="reviews" leftSection={<IconUsersGroup size={14} />}>
+            Reviews
+          </Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel value="vote">
+          <VoteUI setCurrentTab={setCurrentTab} />
+        </Tabs.Panel>
+        <Tabs.Panel value="results">
+          <Results />
+        </Tabs.Panel>
+        <Tabs.Panel value="reviews">
+          <Reviews />
+        </Tabs.Panel>
+      </Tabs>
+    </PageLayout>
+  );
 };
